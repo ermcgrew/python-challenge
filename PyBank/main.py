70a23fe2f36f91674ae47a4b5b715d59b8e7f186
PlanetM <ermcgrew@users.noreply.github.com>
2021-10-04 14:20:46 -0400

    total months for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,9 +4,18 @@ import csv
 #connect to budget_data.csv file
 path = os.path.join("Resources","budget_data.csv")
 
+total_months = 0
+
 with open(path) as file:
     content = csv.reader(file, delimiter=',')
-    print(f"this worked {content}")
+    next(content, None) #skip headers
+    for row in content:
+        #total number of months (count how many rows)
+        total_months = total_months + 1
+        #net profit/loss 
+
+
+print (total_months)
 
 
 
