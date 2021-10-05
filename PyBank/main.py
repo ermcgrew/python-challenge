9d692d009aa53dc9ca85f206c9470d17806b141d
PlanetM <ermcgrew@users.noreply.github.com>
2021-10-05 12:53:30 -0400

    all steps written, work on logic/math for calculations

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,7 +6,7 @@ path = os.path.join("Resources","budget_data.csv")
 
 total_months = 0
 net = 0
-last_row = 0
+last_row_change = 0
 big_inc = 0
 big_dec = 0
 
@@ -16,19 +16,31 @@ with open(path) as file:
     for row in content:
         #total number of months (count how many rows)
         total_months = total_months + 1
+        
         #net profit/loss 
         net = net + int(row[1])
-        #average change = change between each row divided by number of rows
         
-        avg_change = last_row + int(row[1])
-        last_row = int(row[1])
+        #sum of change between each row for average change calculation
+        sum_change = last_row_change + int(row[1])
+        last_row_change = int(row[1])
 
-        if int(row[1]) > big_inc:
+        #check for bigger increase & store month
+        if int(row[1]) >= big_inc:
             big_inc = int(row[1])
+            big_inc_month = row[0]
+        
+        #check for bigger decrease & store month
+        if int(row[1]) <= big_dec:
+            big_dec = int(row[1])
+            big_dec_month = row[0]
+
+#average change = change between each row divided by number of rows
+avg_change = round(sum_change/int(total_months), 2)
+
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_months}")
 print(f"Total: ${net}")
 print(f"Average Change: ${avg_change}")
-print(f"Greatest Increase in Profits: {big_inc}") #name variable
-print(f"Greatest Decrease in Profits: ") #name variable
+print(f"Greatest Increase in Profits: {big_inc_month} (${big_inc})") 
+print(f"Greatest Decrease in Profits: {big_dec_month} (${big_dec})") 
