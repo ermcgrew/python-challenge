840e74cc7bde7ae934d84e97bf1826faf7c8061d
PlanetM <ermcgrew@users.noreply.github.com>
2021-10-05 12:42:31 -0400

    first attempt at greatest increase

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,8 @@ path = os.path.join("Resources","budget_data.csv")
 total_months = 0
 net = 0
 last_row = 0
+big_inc = 0
+big_dec = 0
 
 with open(path) as file:
     content = csv.reader(file, delimiter=',')
@@ -17,13 +19,16 @@ with open(path) as file:
         #net profit/loss 
         net = net + int(row[1])
         #average change = change between each row divided by number of rows
-        last_row = int(row[1])
+        
         avg_change = last_row + int(row[1])
+        last_row = int(row[1])
 
+        if int(row[1]) > big_inc:
+            big_inc = int(row[1])
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_months}")
 print(f"Total: ${net}")
 print(f"Average Change: ${avg_change}")
-print(f"Greatest Increase in Profits: ") #name variable
+print(f"Greatest Increase in Profits: {big_inc}") #name variable
 print(f"Greatest Decrease in Profits: ") #name variable
