6c87d995b2c0246cce97189728942d06bad19e17
PlanetM <ermcgrew@users.noreply.github.com>
2021-10-07 14:36:29 -0400

    pypoll dictionary version completed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,12 +11,14 @@ path=os.path.join("Resources", "election_data.csv")
 candidate_list= []
 votedict = {}
 most_votes = 0
+dash = "\n-------------------------"
 
 #read csv file and store/calculate required information
 with open(path) as location:
     content = csv.reader(location, delimiter=",")
     csv_header = next(content) #store csv headers
     for row in content:
+        #row[2] is candidate's name for each vote cast
         candidate_name = row[2]
         # check for candidate_name in votedict, if not already present, add as key and +1 vote value     
         if candidate_name not in votedict:
@@ -30,31 +32,28 @@ total=sum(votedict.values())
 
 #calculate percentage of votes each candidate won & add to votedict as another value for each key
 for name, votes in votedict.items():
-    votedict[name] = [votes, round(((votes/total) *100), 3)]
+    votedict[name] = [votes, round(((votes/total) *100), 1)]
     #find winner using largest vote value
     if votes >= most_votes: 
         most_votes = votes
         winner = name
 
+
 #print the analysis to the terminal
-print("Election Results")
-print("-------------------------")
-print(f"Total Votes: {total}")
-print("-------------------------")
-
+print(f"Election Results{dash}")
+print(f"Total Votes: {total}{dash}")
 for name, values in votedict.items():
-    #print(f"{name}: {percent}% ({votes})")
-    print(f"{name}: {values}")
+    print(f"{name}: {values[1]}% ({values[0]})")
+print(f"{dash}\nWinner: {winner}{dash}")
 
 
-
-print("-------------------------")
-print(f"Winner: {winner}")
-print("-------------------------")
-
-#export a text file with the results
+#export analysis to a text file
 outpath = os.path.join("analysis", "PyPoll_output.txt")
 
 with open(outpath, "w") as file:
-    file.write ("insert results here")
-    #*****************put in strings for results
+    file.write(f"Election Results{dash}")
+    file.write(f"\nTotal Votes: {total}{dash}")
+    for name, values in votedict.items():
+        file.write("\n")
+        file.write(f"{name}: {values[1]}% ({values[0]})")
+    file.write(f"{dash}\nWinner: {winner}{dash}")
